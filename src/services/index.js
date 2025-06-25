5a6d4b414f262fe1d31d4949264355bc403d2e4b
simonyao <simon.yao97@gmail.com>
2025-06-24 20:12:39 -0500

    updated index

---
+++ src/services/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  chapterService: require('./chapterService'),
+  commentService: require('./commentService'),
+  eventService: require('./eventService'),
+  likeService: require('./likeService'),
+  messageService: require('./messageService'),
+  notificationService: require('./notificationService'),
+  organizationService: require('./organizationService'),
+  organizationAccountService: require('./organizationAccountService'),
+  permissionService: require('./permissionService'),
+  postService: require('./postService'),
+  roleService: require('./roleService'),
+  rolePermissionService: require('./rolePermissionService'),
+  memberService: require('./memberService'),
+  memberAccountService: require('./memberAccountService'),
+  chapterAccountService: require('./chapterAccountService'),
+  friendshipService: require('./friendshipService'),
+};
