50c181d5f1735f1abf69e5bbd1d401c29956f2fd
simonyao <simon.yao97@gmail.com>
2025-06-26 17:56:46 -0500

    fix index

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,16 +1,13 @@
 module.exports = {
-    chapterController: require('./chapterController'),
     commentController: require('./commentController'),
     eventController: require('./eventController'),
     likeController: require('./likeController'),
     messageController: require('./messageController'),
     notificationController: require('./notificationController'),
-    organizationController: require('./organizationController'),
     permissionController: require('./permissionController'),
     postController: require('./postController'),
     roleController: require('./roleController'),
-    rolePermissionController: require('./rolePermissionController'),
-    memberController: require('./memberController'),
     tagController: require('./tagController'),
     accountController: require('./accountController'),
+    friendshipController: require('./friendshipController'),
 };
